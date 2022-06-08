211ad0832d29f9c80c4ae0f3b29073b25039be87
Jasmine Drumright <jmdrumright@gmail.com>
2022-06-07 21:29:23 -0500

    Job title queries are ready for UNIONing, need to make code for year by year for TLC approvals for Q2

--- scripts/tec.sql
+++ scripts/tec.sql
@@ -1,9 +1,12 @@
 -- H-2A Temporary Labor Certification (TLC) data, 2010-2020
 -- Source: https://www.dol.gov/agencies/eta/foreign-labor/performance
--- 119,851 rows
+-- NOTE: This file was accidentally named tec.sql when it should have been tlc.sql, leaving it as is
 
 SELECT COUNT(*)
 FROM main
+-- 119,851 rows
+
+-- CROPS (8): Citrus, strawberries, sheep, cattle, lettuce, potato, tobacco, tomato
 
 -- View entire data set (by year)
 
@@ -21,212 +24,221 @@ FROM main
 WHERE RIGHT(begin_date,2) = '10' -- Year filter, omit for all years
 
 
--- Count of all withdrawn or denied cases (3961)
+-- Count of all withdrawn or denied cases (3961 = 3.3%)
 
-SELECT COUNT(case_status) as dw_total,
+SELECT COUNT(case_status) as dw_total
 FROM main
 WHERE case_status ILIKE '%deni%'
 	OR case_status ILIKE '%withdr%'
 
 
 ----------------------------------------
--- FRUITS
+-- ALL CROPS (8)
 ----------------------------------------
 
--- Apples, total workers (no 11)
+-- Citrus (grapefruit, lemon, orange), total workers
 
-SELECT RIGHT(begin_date,2) AS year,
+WITH cit AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as apple_total,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
-WHERE job_title ILIKE '%apple%' AND job_title NOT ILIKE '%grapple%'
--- 	AND RIGHT(begin_date,2) = '10'
+WHERE job_title ILIKE '%citrus%'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
 
-
--- Blueberries, total workers (no 10-11)
-
-SELECT RIGHT(begin_date,2) AS year,
+SELECT year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as blueberry_total,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
 	employer_state,
 	worksite_state
-FROM main
-WHERE job_title ILIKE '%blueberr%'
--- 	AND RIGHT(begin_date,2) = '10'
-GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
-
-
--- Grapes, total workers (no 11)
-
-SELECT RIGHT(begin_date,2) AS year,
-	job_title,
-	workers_req,
-	SUM(workers_req::FLOAT) OVER() as grape_total,
-	employer_state,
-	worksite_state
-FROM main
-WHERE job_title ILIKE '%grape%' AND job_title NOT ILIKE 'grapefruit'
--- AND RIGHT(begin_date,2) = '10'
-GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
-
-
--- Melons, total workers (no 10-11)
-
-SELECT RIGHT(begin_date,2) AS year,
-	job_title,
-	workers_req,
-	SUM(workers_req::FLOAT) OVER() as melon_total,
-	employer_state,
-	worksite_state
-FROM main
-WHERE job_title ILIKE '%melon%'
--- AND RIGHT(begin_date,2) = '10'
-GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
-
-
--- Oranges, total workers (no 10-11)
-
-SELECT RIGHT(begin_date,2) AS year,
-	job_title,
-	workers_req,
-	SUM(workers_req::FLOAT) OVER() as orange_total,
-	employer_state,
-	worksite_state
-FROM main
-WHERE job_title ILIKE '%orange%'
--- AND RIGHT(begin_date,2) = '10'
-GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+FROM cit
 
 
 -- Strawberries, total workers
 
-SELECT RIGHT(begin_date,2) AS year,
+WITH str AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as strawberry_total,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
 WHERE job_title ILIKE '%strawberr%'
--- AND RIGHT(begin_date,2) = '10'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
 
-
--- Sugarcane, total workers (no 11)
-
-SELECT RIGHT(begin_date,2) AS year,
+SELECT year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as sugarcane_total,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM str
+
+
+-- Sheep, total workers
+
+WITH she AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
+	job_title,
+	workers_req,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
-WHERE job_title ILIKE '%sugarcane%'
--- AND RIGHT(begin_date,2) = '10'
+WHERE job_title ILIKE '%sheep%'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
 
-
--- Raspberries, total workers (no 10, 12)
-
-SELECT RIGHT(begin_date,2) AS year,
+SELECT year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as raspberry_total,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM she
+
+
+-- Cattle, total workers
+
+WITH cat AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
+	job_title,
+	workers_req,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
-WHERE job_title ILIKE '%raspberr%'
--- AND RIGHT(begin_date,2) = '10'
+WHERE job_title ILIKE '%cattle%'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
 
-
-----------------------------------------
--- VEGETABLES
-----------------------------------------
-
--- Corn, total workers (no 11-12)
-
-SELECT RIGHT(begin_date,2) AS year,
+SELECT year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as corn_total,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
 	employer_state,
 	worksite_state
-FROM main
-WHERE job_title ILIKE '%corn%'
--- AND RIGHT(begin_date,2) = '10'
-GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+FROM cat
 
 
 -- Lettuce, total workers
 
-SELECT RIGHT(begin_date,2) AS year,
+WITH let AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as lettuce_total,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
 WHERE job_title ILIKE '%lettuce%'
--- AND RIGHT(begin_date,2) = '10'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
 
-
--- Potato, total workers
-
-SELECT RIGHT(begin_date,2) AS year,
+SELECT year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as potato_total,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM let
+
+
+-- Potato (potato, sweet potato), total workers
+
+WITH pot AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
+	job_title,
+	workers_req,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
 WHERE job_title ILIKE '%potato%'
--- AND RIGHT(begin_date,2) = '10'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
+
+SELECT year,
+	job_title,
+	workers_req,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM pot
 
 
 -- Tobacco, total workers
 
-SELECT RIGHT(begin_date,2) AS year,
+WITH tob AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as tobacco_total,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
 WHERE job_title ILIKE '%tobacco%'
--- AND RIGHT(begin_date,2) = '10'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
+
+SELECT year,
+	job_title,
+	workers_req,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM tob
 
 
 -- Tomato, total workers
 
-SELECT RIGHT(begin_date,2) AS year,
+WITH tom AS (
+SELECT
+	(RIGHT(begin_date,2)::int) AS year,
 	job_title,
 	workers_req,
-	SUM(workers_req::FLOAT) OVER() as test_total,
+	SUM(workers_req::FLOAT) OVER() as wr_total,
 	employer_state,
 	worksite_state
 FROM main
-WHERE job_title ILIKE '%tomato%' AND job_title NOT ILIKE '%cherry%'
--- AND RIGHT(begin_date,2) = '10'
+WHERE job_title ILIKE '%tomato%'
+	AND (RIGHT(begin_date,2)::int) >= 10
 GROUP BY year, job_title, employer_state, workers_req, worksite_state
-ORDER BY year
+ORDER BY year)
+
+SELECT year,
+	job_title,
+	workers_req,
+	wr_total,
+	SUM(workers_req::FLOAT) OVER(PARTITION BY year) AS wr_total_year,
+	employer_state,
+	worksite_state
+FROM tom
